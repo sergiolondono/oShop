b11747ba1ff9c195b7b25908c1d2287adbc5c014
Londono Gallo Sergio Andres <Sergio.Londono@carvajal.com>
2019-05-16 12:29:33 -0500

    20190516

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyClnKbi_dsXpMQMHeiIEfxpwDCQXbpHs4k",
+    authDomain: "oshop-42dda.firebaseapp.com",
+    databaseURL: "https://oshop-42dda.firebaseio.com",
+    projectId: "oshop-42dda",
+    storageBucket: "oshop-42dda.appspot.com",
+    messagingSenderId: "187674126963",
+    appId: "1:187674126963:web:1e580fdf333b9931"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyClnKbi_dsXpMQMHeiIEfxpwDCQXbpHs4k",
+    authDomain: "oshop-42dda.firebaseapp.com",
+    databaseURL: "https://oshop-42dda.firebaseio.com",
+    projectId: "oshop-42dda",
+    storageBucket: "oshop-42dda.appspot.com",
+    messagingSenderId: "187674126963",
+    appId: "1:187674126963:web:1e580fdf333b9931"
+  }
 };
 
 /*
