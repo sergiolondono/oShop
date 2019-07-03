437e82aae8a96d1d57f5edb2260a0df1d755cc2e
Londono Gallo Sergio Andres <Sergio.Londono@carvajal.com>
2019-07-03 15:07:51 -0500

    20190703. Product Catalog Module. Video 4.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -57,7 +57,7 @@ import { CategoryService } from './category.service';
     AngularFireAuthModule,
     NgbModule.forRoot(),
     RouterModule.forRoot([
-      { path: '', component: HomeComponent },
+      { path: '', component: ProductsComponent },
       { path: 'products', component: ProductsComponent },
       { path: 'shopping-cart', component: ShoppingCartComponent },
       { path: 'login', component: LoginComponent },
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { CategoryService } from "./../category.service";
+import { ProductService } from "./../product.service";
+import { Component } from "@angular/core";
+import { Product } from "../models/product";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  selector: "app-products",
+  templateUrl: "./products.component.html",
+  styleUrls: ["./products.component.css"]
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
+  products: any[] = [];
+  filteredProducts: any[] = [];
+  categories$;
+  category: string;
 
-  constructor() { }
+  constructor(
+    route: ActivatedRoute,
+    productService: ProductService,
+    categoryService: CategoryService
+  ) {
+    productService.getAll().subscribe(products => {
+      this.products = products;
+    });
+    console.log(this.products);
 
-  ngOnInit() {
+    this.categories$ = categoryService.getAll();
+
+    route.queryParamMap.subscribe(params => {
+      this.category = params.get("category");
+
+      this.filteredProducts = this.category
+        ? this.products.filter(p => p.category === this.category)
+        : this.products;
+    });
   }
-
 }
