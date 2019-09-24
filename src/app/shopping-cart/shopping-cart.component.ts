dd82bc67284d7ea3d542db13da1f20835004825f
Sergio Andres Londono Gallo <Sergio.Londono@carvajal.com>
2019-09-24 09:38:47 -0500

    20190924. Shopping cart detail.

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from "./../shopping-cart.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-shopping-cart',
-  templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.css']
+  selector: "app-shopping-cart",
+  templateUrl: "./shopping-cart.component.html",
+  styleUrls: ["./shopping-cart.component.css"]
 })
 export class ShoppingCartComponent implements OnInit {
+  cart$;
+  shoppingCartItem;
+  shoppingCartItemCount: number;
+  products;
+  objectKeys;
 
-  constructor() { }
+  constructor(private shoppingCartService: ShoppingCartService) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.cart$ = await this.shoppingCartService.getCart();
+    this.cart$.subscribe(cart => {
+      this.shoppingCartItem = cart;
+      this.shoppingCartItemCount = this.shoppingCartService.totalItemsCount(
+        this.shoppingCartItem
+      );
+
+      for (let item in cart) {
+        if (cart[item].key === "items") {
+          this.products = cart[item];
+          console.log(this.products);
+          this.objectKeys = Object.keys(this.products);
+          console.log(this.objectKeys);
+        }
+      }
+    });
   }
-
 }
